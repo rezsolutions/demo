4ba6e6534e64698eec4182d2c6e48d4d40a0d50d
Ultim8Pedigree <ultim8pedigree@gmail.com>
2025-02-16 16:11:24 -0500

    summary added

--- ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppDemo
 {
+    /// <summary>
+    /// Demo summary
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
